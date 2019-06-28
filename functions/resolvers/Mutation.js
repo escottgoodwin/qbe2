dff410db1e3ee948a318153217f9f332a17ae80c
Evan Goodwin <escottgoodwin@gmail.com>
2019-06-28 15:05:20 -0700

    publish test short

--- functions/resolvers/Mutation.js
+++ functions/resolvers/Mutation.js
@@ -1146,7 +1146,7 @@ async function publishTestShort(parent, args, ctx, info) {
         },
       },
     },
-    `{ id question correctShortAnswer test { subject id } panel { id link }  }`
+    `{ id }`
   )
 
   return test
